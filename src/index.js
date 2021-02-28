3e3943ed75fa8f4ba93a7b8f85d5e0ef50667e07
timeonator <timeonator@gmail.com>
2021-02-27 19:38:17 -0800

    added user meta-data to profile and config

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+const authConfig = require("./auth0-config.json");
+
 
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-enct0se6.us.auth0.com"
-    clientId="LXzrHteJ8W6cJ1ilskxz2dP1rUC73Rjq"
+    domain={authConfig.domain}
+    clientId={authConfig.clientId}
     redirectUri={window.location.origin}
   >
+     
     <App />
   </Auth0Provider>,
   document.getElementById("root")
