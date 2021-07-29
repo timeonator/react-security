32300e9a3800914ae50025b0d5a1f23cca9a9c1f
timeonator <timeonator@gmail.com>
2021-07-29 01:46:27 -0700

    navigation

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,22 +2,19 @@ import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 // import { useAuth0 } from "@auth0/auth0-react";
-import LoginButton  from "./login-button";
-import LogoutButton from "./logout-button";
 import Profile from "./profile";
 import Navigation from './components/navigation'
 
-
 function App() {
     return (
-    <div className="App">
+    <React.StrictMode>
+      <div className="App">
         <header>
             <Navigation />            
         </header>
-
         <Profile />
-    </div>
+    </div>          
+    </React.StrictMode>
   );
 }
-
 export default App;
