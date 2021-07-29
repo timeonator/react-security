94e8e989b5dd2c6c35d488628b2869f4fc9a9f1b
timeonator <timeonator@gmail.com>
2021-07-29 01:26:02 -0700

    bootstrap css

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 const authConfig = require("./auth0-config.json");
