756f8fd90f2436870cd74a71bfa1788341a44081
timeonator <timeonator@gmail.com>
2021-07-29 01:24:17 -0700

    initial checkin page stubs

---
+++ Client/src/components/about.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const About = () => (
+    <div className="about">
+        <h4>About</h4>
+    </div>
+);
+export default About;
---
+++ Client/src/components/contact.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Contact = () => (
+    <div className="about">
+        <h4>Contact</h4>
+    </div>
+);
+export default Contact;
