9a11aa4e47f231db27ec0c8b785ccee6dbf78f26
timeonator <timeonator@gmail.com>
2021-07-29 01:26:58 -0700

    styling with bootstrap

--- Client/src/components/navigation.js
+++ Client/src/components/navigation.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Nav} from 'react-bootstrap'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
+import About from './about'
+import Contact from './contact'
+import Home from './home'
 
 const Navigation = (props) => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
   return (
+    <Router>
     <div className="header">
-      {!isAuthenticated && (
-        <ul className="nav nav-pills pull-right">
-          <button onClick={() => loginWithRedirect({})}>Log in</button>
-        </ul>
-      )}
-
+        {!isAuthenticated && (
+            <Nav className="nav nav-pills pull-right">
+                <Nav.Link href="/about">About</Nav.Link>
+                <button onClick={() => loginWithRedirect({})}>Log in</button>
+            </Nav>
+        )}
+        <h3 className="text-muted">Secure</h3>
       {isAuthenticated && (
-        <ul className="nav nav-pills pull-right">
-          {/* <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li> */}
+        <Nav className="nav nav-pills pull-right">
+          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
           <button onClick={() => logout()}>Logout</button>
-        </ul>
+        </Nav>
       )}
-      <h3 className="text-muted">Securing React</h3>
+                <Switch>
+            <Route path="/contact" component={Contact}/>
+            <Route path="/about" component={About}/>
+            <Route path="/home" component={Home}/>
+            <Route exact path="/" component={About}/>
+          </Switch>
     </div>
+    </Router>
+
   );
 }
-
 export default Navigation; 
