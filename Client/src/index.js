31f48e41d1d13d9740431232ceb78a713e5dca67
timeonator <timeonator@gmail.com>
2021-08-03 07:39:00 -0700

    spinning my own AuthoProvider and wrapping the app

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,18 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
-const authConfig = require("./auth0-config.json");
+import { Auth0Provider } from './react-auth0-spa';
+const authConfig = require('./auth0-config');
 
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
 ReactDOM.render(
   <Auth0Provider
     domain={authConfig.domain}
-    clientId={authConfig.clientId}
-    redirectUri={window.location.origin}
+    client_id={authConfig.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
   >
-     
     <App />
   </Auth0Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
